9f80fa691131c3094bd986ca53aafabd808fdb20
Emerson Melo Martins <emerson25xd@gmail.com>
2025-08-06 21:54:12 -0300

    queries com knex

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,22 @@ import { knex } from "./database";
 const app = fastify();
 
 app.get("/hello", async () => {
-  const tables = await knex("sqlite_schema").select("*");
+  // const tables = await knex("sqlite_schema").select("*");
+  // return tables;
 
-  return tables;
+  // const transaction = await knex("transactions")
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: "Transação de teste",
+  //     amount: 1000,
+  //   })
+  //   .returning("*");
+
+  const transactions = await knex("transactions")
+    .where("amount", 1000)
+    .select("*");
+
+  return transactions;
 });
 
 app
