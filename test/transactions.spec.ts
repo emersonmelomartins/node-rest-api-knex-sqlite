f7461a15b017a0e3e720658e983a6fc3e4f2defa
Emerson Melo Martins <emerson25xd@gmail.com>
2025-08-11 20:00:27 -0300

    Testando criação e retorno de lista baseado em cookie criado

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,5 +1,5 @@
 import supertest from "supertest";
-import { afterAll, beforeAll, describe, test } from "vitest";
+import { afterAll, beforeAll, describe, expect, test } from "vitest";
 import { app } from "../src/app";
 
 describe("Transactions routes", () => {
@@ -11,8 +11,10 @@ describe("Transactions routes", () => {
     await app.close();
   });
 
+  // Todo teste deve excluir o contexto, não pode ter um teste dependendo do resultado de outro teste
+  // Se caso tiver que utilizar dois testes, deve ser feito no mesmo contexto
   test("user can create a new transaction", async () => {
-    await supertest(app.server)
+    const response = await supertest(app.server)
       .post("/transactions")
       .send({
         title: "New Transaction",
@@ -20,5 +22,30 @@ describe("Transactions routes", () => {
         type: "credit",
       })
       .expect(201);
+  });
+
+  test("should be able to list all transactions", async () => {
+    const createTransactionResponse = await supertest(app.server)
+      .post("/transactions")
+      .send({
+        title: "New Transaction",
+        amount: 5000,
+        type: "credit",
+      });
+
+    const cookies = createTransactionResponse.get("Set-Cookie") ?? [];
+
+    const listTransactionsResponse = await supertest(app.server)
+      .get("/transactions")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: "New Transaction",
+        amount: 5000,
+        id: expect.any(String),
+      }),
+    ]);
   });
 });
